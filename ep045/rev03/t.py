7de34f59dd29bd1d3d2d708497b4e068ab193042
int3l <int3l@users.noreply.github.com>
2020-12-06 20:04:21 +0200

    Add code examples for episode 045 & 046

---
+++ ep045/rev03/t.py
@@ -0,0 +1,34 @@
+from __future__ import annotations  # python3.7+
+
+from typing import Generic
+from typing import overload
+from typing import Sequence
+from typing import TypeVar
+from typing import Union
+
+
+T = TypeVar('T')
+
+
+class C(Generic[T]):
+    def __init__(self, lst: Sequence[T]) -> None:
+        self._lst = lst
+
+    @overload
+    def __getitem__(self, idx: int) -> T: ...
+    @overload
+    def __getitem__(self, idx: slice) -> C[T]: ...
+
+    def __getitem__(self, idx: Union[slice, int]) -> Union[C[T], T]:
+        if isinstance(idx, slice):
+            return C(self._lst[idx])
+        else:
+            return self._lst[idx]
+
+    def __repr__(self) -> str:
+        return f'{type(self).__name__}({self._lst})'
+
+
+inst = C([1, 2, 3])
+reveal_type(inst[1:])
+reveal_type(inst[1])
