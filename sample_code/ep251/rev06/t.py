a143d02d56504aa6c63a1313c7b20a8c022d9229
int3l <int3l@users.noreply.github.com>
2021-04-10 17:22:01 +0300

    Add code examples for episode 251 & 252

---
+++ sample_code/ep251/rev06/t.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import functools
+
+
+@functools.total_ordering
+class C:
+    def __init(self, x: int) -> None:
+        self.x = x
+
+    def __repr(self) -> str:
+        return f'{type(self).__name__}({self.x})'
+
+    def __lt__(self, other: object) -> bool:
+        if isinstance(other, C):
+            return self.x < other.x
+        else:
+            return NotImplemented
+
+
+@functools.total_ordering
+class D:
+    def __init(self, y: int) -> None:
+        self.y = y
+
+    def __repr(self) -> str:
+        return f'{type(self).__name__}({self.y})'
+
+    def __lt__(self, other: object) -> bool:
+        if isinstance(other, C):
+            return self.y < other.x
+        elif isinstance(other, D):
+            return self.y < other.y
+        else:
+            return NotImplemented
---
+++ sample_code/ep252/rev03/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+    name='all_repos_stubs',
+    version='...',
+    packages=['all_repos-stubs'],
+    # install_requires=['all_repos>=...'],
+)
