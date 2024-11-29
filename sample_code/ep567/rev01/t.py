05f448e326076ca850defd05717bd97047ed280f
Bozho Dimitrov <1278836+bozhodimitrov@users.noreply.github.com>
2024-11-29 18:55:49 +0200

    Update t.py

--- sample_code/ep567/rev01/t.py
+++ sample_code/ep567/rev01/t.py
@@ -1,11 +1,16 @@
 from sentry.buffer import backend
 from sentry.models.group import Group
+from sentry.models.organization import Organization
 from sentry.models.project import Project
+from sentry.models.release import Release
+from sentry.models.releases.release_project import ReleaseProject
+from sentry.models.team import Team
+
 
 group = Group.objects.create(project=Project(id=1))
 org = Organization.objects.create(slug="test-org")
 team = Team.objects.create(organization=org, slug="test-team")
-prject = Project.objects.create(organization=org, slug="test-project")
+project = Project.objects.create(organization=org, slug="test-project")
 project.add_team(team)
 release = Release.objects.create(organization=org, version="abcdefg")
 release_project = ReleaseProject.objects.create(project, release=release)
