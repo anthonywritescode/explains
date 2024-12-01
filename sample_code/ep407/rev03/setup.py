eaba750ee169090f41ecc3e7745a02294a9dfc61
Dennis Kasper <dennis.m.kasper@gmail.com>
2024-12-01 15:07:46 +0100

    fix typo in setup.py

--- sample_code/ep407/rev03/setup.py
+++ sample_code/ep407/rev03/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='example',
     packages=find_packages(),
-    py_module=['b'],
+    py_modules=['b'],
     entry_points={
         'console_scripts': ['a-main=a.main:main'],
     },
