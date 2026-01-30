5d4cd41fe528610db378f5de91694da80bfb61d4
Bozho Dimitrov <1278836+bozhodimitrov@users.noreply.github.com>
2026-01-30 18:31:46 +0200

    Add examples for episode 604 & 605

---
+++ sample_code/ep605/rev03/t.py
@@ -0,0 +1,11 @@
+from typing import LiteralString
+
+
+def to_jsonb(table: LiteralString, column: LiteralString) -> str:
+    return f'''\
+        ALTER TABLE {table} ALTER COLUMN {column}
+        TYPE jsonb USING {column}::jsonb;
+    '''
+
+
+print(to_jsonb('users', 'metadata'))
---
+++ sample_code/ep605/rev04/t2.py
@@ -0,0 +1,20 @@
+from typing import LiteralString
+
+LANG = 'es_US'
+
+TRANSLATIONS = {
+    'I love {language}': {
+        'en_US': 'I love {language}',
+        'es_US': 'a mi me gusta {language}',
+    }
+}
+
+
+def _(x: LiteralString, **params: str) -> str:
+    format_str = TRANSLATIONS.get(x, {}).get(LANG, x)
+    return format_str.format(**params)
+
+
+language = input('what is your favorite language? ')
+
+print(_('I love {language}'.format(language=language)))
